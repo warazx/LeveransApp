53e7144683815a47096f0d5781f170ab2d428831
Christian Karlsson <krille.karlsson@gmail.com>
2016-11-14 11:17:57 +0100

    Basic fields and constructor for the Order class.

--- app/src/main/java/com/iths/grupp1/leveransapp/model/Order.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/model/Order.java
@@ -5,5 +5,26 @@ package com.iths.grupp1.leveransapp.model;
  */
 
 public class Order {
+    private int orderNumber;
+    private int orderSum;
+    private Customer customer;
+    private boolean isDelivered;
+    private long orderPlacementDate;
+    private long deliveryDate;
+    private long deliveryLatitude;
+    private long deliveryLongitude;
 
+    private static int orderID = 1;
+
+    public Order(Customer customer) {
+        this.orderNumber = orderID;
+        orderID++;
+        this.customer = customer;
+        this.isDelivered = false;
+        //TODO: Get the current time from the helper class.
+        this.orderPlacementDate = 1000; //Placeholder.
+        this.deliveryDate = 0;
+        this.deliveryLatitude = 0;
+        this.deliveryLongitude = 0;
+    }
 }
