892574254c2b881aef5c14d26f5fb43f8f9e511a
Christian Karlsson <krille.karlsson@gmail.com>
2016-11-14 11:35:27 +0100

    .

--- app/src/main/java/com/iths/grupp1/leveransapp/database/OrderSQLiteOpenHelper.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/database/OrderSQLiteOpenHelper.java
@@ -1,13 +1,9 @@
 package com.iths.grupp1.leveransapp.database;
 
-import com.iths.grupp1.leveransapp.model.Customer;
-
 /**
  * Created by christiankarlsson on 14/11/16.
  */
 
 public class OrderSQLiteOpenHelper {
-    private int orderNumber;
-    private int orderSum;
-    private Customer customer;
+    
 }
