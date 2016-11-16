551b539e8eaf6e2849013ed072d2b80fc70dfd16
Christian Karlsson <krille.karlsson@gmail.com>
2016-11-16 08:57:32 +0100

    Added an botton on the loginscreen so i can get to the orderActivity.

--- app/src/main/java/com/iths/grupp1/leveransapp/view/LoginActivity.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/view/LoginActivity.java
@@ -1,15 +1,30 @@
 package com.iths.grupp1.leveransapp.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.iths.grupp1.leveransapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //Testbutton for Orders.
+    private Button loginBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Testbutton for Orders.
+        loginBtn = (Button) findViewById(R.id.login_btn);
+    }
+
+
+    public void goToOrders(View view) {
+        Intent intent = new Intent(this, OrderListActivity.class);
+        startActivity(intent);
     }
 }
