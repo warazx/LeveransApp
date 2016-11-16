53c4d3c3c702c32193ed6f607f10dfec231364b9
Christian Karlsson <krille.karlsson@gmail.com>
2016-11-16 12:06:54 +0100

    Added function to test the Activity and the items.

--- app/src/main/java/com/iths/grupp1/leveransapp/view/OrderListActivity.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/view/OrderListActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 
 import com.iths.grupp1.leveransapp.R;
 import com.iths.grupp1.leveransapp.adapter.OrderAdapter;
+import com.iths.grupp1.leveransapp.model.Customer;
 import com.iths.grupp1.leveransapp.model.Order;
 
 import java.util.Arrays;
@@ -19,7 +20,7 @@ public class OrderListActivity extends AppCompatActivity {
 
     public static final String LISTED_ORDERS = "LISTED_ORDERS";
 
-    private Order[] orders;
+    private Order[] orders = new Order[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +28,14 @@ public class OrderListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order_list);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
+        /*
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(LISTED_ORDERS);
-        orders = Arrays.copyOf(parcelables, parcelables.length, Order[].class);
+        orders = Arrays.copyOf(parcelables, parcelables.length, Order[].class);*/
+        //TEMP while intent is not finished.
+        orders[0] = new Order(new Customer(43243, "083-142455433", "Storgatan 10"));
+        orders[1] = new Order(new Customer(23574, "070-333667899", "Rosstigen 7"));
+        //-------END TEMP
 
         OrderAdapter adapter = new OrderAdapter(orders);
         recyclerView.setAdapter(adapter);
