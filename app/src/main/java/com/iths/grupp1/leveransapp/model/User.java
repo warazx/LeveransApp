103f2c84990a69f2fba969b362771fef1e0fea44
Christian Karlsson <krille.karlsson@gmail.com>
2016-11-16 10:23:24 +0100

    Added the User class that will be used to log into the app.

---
+++ app/src/main/java/com/iths/grupp1/leveransapp/model/User.java
@@ -0,0 +1,15 @@
+package com.iths.grupp1.leveransapp.model;
+
+/**
+ * Created by christiankarlsson on 16/11/16.
+ */
+
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
