d2ae5a611e6a01d3083f3e4aee92e3a0b2240669
Christian Karlsson <krille.karlsson@gmail.com>
2016-11-16 11:56:06 +0100

    Added getters and setters for the Customer class.

--- app/src/main/java/com/iths/grupp1/leveransapp/model/Customer.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/model/Customer.java
@@ -16,4 +16,28 @@ public class Customer {
         this.address = address;
         this.creatonDate = System.currentTimeMillis();
     }
+
+    public int getCostumerNumber() {
+        return costumerNumber;
+    }
+
+    public void setCostumerNumber(int costumerNumber) {
+        this.costumerNumber = costumerNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
