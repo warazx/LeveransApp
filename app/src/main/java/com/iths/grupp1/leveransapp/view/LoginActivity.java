42d7e61e7c7166fecaa81bd43f4329ffbc7354c3
Christian Karlsson <krille.karlsson@gmail.com>
2016-11-16 12:45:24 +0100

    Renamed main to login.

--- app/src/main/java/com/iths/grupp1/leveransapp/view/LoginActivity.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/view/LoginActivity.java
@@ -16,7 +16,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         //Testbutton for Orders.
         loginBtn = (Button) findViewById(R.id.login_btn);
