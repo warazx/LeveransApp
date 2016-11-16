dab0b8048d871e529519d65b45e73563bdde262e
Christian Karlsson <krille.karlsson@gmail.com>
2016-11-16 12:26:47 +0100

    Added utility class to generate orders.

---
+++ app/src/main/java/com/iths/grupp1/leveransapp/util/GenerateOrders.java
@@ -0,0 +1,21 @@
+package com.iths.grupp1.leveransapp.util;
+
+import com.iths.grupp1.leveransapp.model.Customer;
+import com.iths.grupp1.leveransapp.model.Order;
+
+/**
+ * Created by christiankarlsson on 16/11/16.
+ */
+
+public final class GenerateOrders {
+
+    public Order[] get(int amount) {
+        Order[] orders = new Order[amount];
+
+        for(int i = 0; i < orders.length; i++) {
+            orders[i] = new Order(new Customer(1, null, null));
+        }
+
+        return orders;
+    }
+}
