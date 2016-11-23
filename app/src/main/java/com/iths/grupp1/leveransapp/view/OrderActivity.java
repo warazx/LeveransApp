282a9c151bb783e02c11c91ba42806834c546c9b
Christian Karlsson <krille.karlsson@gmail.com>
2016-11-23 16:08:46 +0100

    Fixed the button to deliver the order.

--- app/src/main/java/com/iths/grupp1/leveransapp/view/OrderActivity.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/view/OrderActivity.java
@@ -41,24 +41,38 @@ public class OrderActivity extends AppCompatActivity {
         placedDateText = (TextView) findViewById(R.id.order_activity_orderid_value);
         deliveryAddressText = (TextView) findViewById(R.id.order_activity_delivery_value);
         phoneNumberText = (TextView) findViewById(R.id.order_activity_phone_value);
+        deliveredDateText = (TextView) findViewById(R.id.order_activity_delivered_date_value);
+        deliveredImage = (ImageView) findViewById(R.id.order_activity_delivered_image);
+        deliveryBtn = (Button) findViewById(R.id.order_activity_delivery_btn);
 
-        if(order.isDelivered()) {
-            findViewById(R.id.ll_delivered).setVisibility(View.VISIBLE);
-            findViewById(R.id.ll_not_delivered).setVisibility(View.GONE);
-            deliveredDateText = (TextView) findViewById(R.id.order_activity_delivered_date_value);
-            deliveredImage = (ImageView) findViewById(R.id.order_activity_delivered_image);
-        } else {
-            findViewById(R.id.ll_delivered).setVisibility(View.GONE);
-            findViewById(R.id.ll_not_delivered).setVisibility(View.VISIBLE);
-            deliveryBtn = (Button) findViewById(R.id.order_activity_delivery_btn);
-        }
+        toggleLayout();
 
         orderIdText.setText(order.getOrderNumber() + "");
         placedDateText.setText(order.getOrderPlacementDate() + "");
         deliveryAddressText.setText(order.getCustomer().getAddress());
         phoneNumberText.setText(order.getCustomer().getPhoneNumber());
+        deliveredDateText.setText(order.getDeliveryDate() + "");
+    }
+
+    private void toggleLayout() {
+        if(order.isDelivered()) {
+            findViewById(R.id.ll_delivered).setVisibility(View.VISIBLE);
+            findViewById(R.id.ll_not_delivered).setVisibility(View.GONE);
+        } else {
+            findViewById(R.id.ll_delivered).setVisibility(View.GONE);
+            findViewById(R.id.ll_not_delivered).setVisibility(View.VISIBLE);
+        }
     }
 
     public void deliverOrder(View view) {
+        order.setDelivered(true);
+        long time = System.currentTimeMillis();
+        order.setDeliveryDate(time);
+        //TODO: Method to get the coordinates from the gps.
+        order.setDeliveryLatitude(111);
+        order.setDeliveryLongitude(111);
+        //TODO: Change to get the formatted date.
+        deliveredDateText.setText(order.getDeliveryDate() + "");
+        toggleLayout();
     }
 }
