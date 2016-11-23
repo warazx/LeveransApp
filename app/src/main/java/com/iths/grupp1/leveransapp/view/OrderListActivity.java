082e00d9af2c0c91cb228faccc4fdf9053616d9b
Christian Karlsson <krille.karlsson@gmail.com>
2016-11-23 16:16:36 +0100

    Cleanup and added a 1 more test order.

--- app/src/main/java/com/iths/grupp1/leveransapp/view/OrderListActivity.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/view/OrderListActivity.java
@@ -1,7 +1,5 @@
 package com.iths.grupp1.leveransapp.view;
 
-import android.content.Intent;
-import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,17 +9,12 @@ import com.iths.grupp1.leveransapp.R;
 import com.iths.grupp1.leveransapp.adapter.OrderAdapter;
 import com.iths.grupp1.leveransapp.model.Customer;
 import com.iths.grupp1.leveransapp.model.Order;
-import com.iths.grupp1.leveransapp.util.GenerateOrders;
-
-import java.util.Arrays;
 
 public class OrderListActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
-
-    public static final String ORDER_STATUS = "ORDER_STATUS";
 
     private Order[] orders;
 
@@ -32,8 +25,9 @@ public class OrderListActivity extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
         //Used for testing, supposed to get all the orders from the database.
-        orders = new Order[1];
+        orders = new Order[2];
         orders[0] = new Order(new Customer(0, "070-12312312", "Storgatan 10"));
+        orders[1] = new Order(new Customer(1, "070-32132132", "Lillagatan 22"));
         //-------------------------------------------------------------------
 
         recyclerView.setHasFixedSize(true);
