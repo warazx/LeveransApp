215b82c99ecec7e7307a6c65c6a82d4640f25f2c
Christian Karlsson <krille.karlsson@gmail.com>
2016-11-24 10:42:05 +0100

    Added (temp) buttons for the order list activity to help display/import orders.

--- app/src/main/java/com/iths/grupp1/leveransapp/util/GenerateDatabaseObject.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/util/GenerateDatabaseObject.java
@@ -1,19 +1,19 @@
 package com.iths.grupp1.leveransapp.util;
 
-import com.iths.grupp1.leveransapp.model.Customer;
 import com.iths.grupp1.leveransapp.model.Order;
 
 /**
  * Created by christiankarlsson on 16/11/16.
  */
 
-public final class GenerateOrders {
+public final class GenerateDatabaseObject {
 
-    public static Order[] get(int amount) {
+    public static Order[] addOrders(int amount) {
         Order[] orders = new Order[amount];
 
         for(int i = 0; i < orders.length; i++) {
-            orders[i] = new Order(new Customer(1, null, null));
+            //TODO: get a random customer.
+            orders[i] = new Order(1);
         }
 
         return orders;
--- app/src/main/java/com/iths/grupp1/leveransapp/view/OrderListActivity.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/view/OrderListActivity.java
@@ -4,11 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.iths.grupp1.leveransapp.R;
 import com.iths.grupp1.leveransapp.adapter.OrderAdapter;
-import com.iths.grupp1.leveransapp.model.Customer;
 import com.iths.grupp1.leveransapp.model.Order;
+import com.iths.grupp1.leveransapp.util.GenerateDatabaseObject;
 
 public class OrderListActivity extends AppCompatActivity {
 
@@ -24,11 +25,13 @@ public class OrderListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order_list);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
-        //Used for testing, supposed to get all the orders from the database.
+
+        /*  TODO: Get all undelivered orders from the database.
+        Used for testing, supposed to get all the orders from the database.*/
         orders = new Order[2];
-        orders[0] = new Order(new Customer(0, "070-12312312", "Storgatan 10"));
-        orders[1] = new Order(new Customer(1, "070-32132132", "Lillagatan 22"));
-        //-------------------------------------------------------------------
+        orders[0] = new Order(1);
+        orders[1] = new Order(1);
+        // ----------------------------------------------------------------
 
         recyclerView.setHasFixedSize(true);
 
@@ -38,4 +41,31 @@ public class OrderListActivity extends AppCompatActivity {
         adapter = new OrderAdapter(orders);
         recyclerView.setAdapter(adapter);
     }
+
+    public void addOrders(View view) {
+        /*  TODO: Get the number from settings.
+            TODO: Move this to the toolbar.*/
+        int amount = 10;
+        Order[] newOrders = GenerateDatabaseObject.addOrders(amount);
+        /*  TODO: Write to the new orders to the database.
+        writeToDatabase(newOrders);
+         */
+    }
+
+    public void updateOrders(View view) {
+        /*  TODO: Get all undelivered orders from the database.
+            TODO: Move this to the toolbar.
+        orders = getUndeliveredOrders();
+        */
+        adapter.notifyDataSetChanged();
+    }
+
+    public void historyOrders(View view) {
+        /*  TODO: Get all delivered orders from the database.
+            TODO: Move this to the toolbar.
+        orders = getDeliveredOrders();
+        */
+        adapter.notifyDataSetChanged();
+    }
+
 }
