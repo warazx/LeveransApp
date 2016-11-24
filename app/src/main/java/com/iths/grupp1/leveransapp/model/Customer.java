62c9bc4ec4230fbbd36c63afdc055e101539af65
Christian Karlsson <krille.karlsson@gmail.com>
2016-11-24 09:43:25 +0100

    Added a String with a name.

--- app/src/main/java/com/iths/grupp1/leveransapp/model/Customer.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/model/Customer.java
@@ -9,12 +9,14 @@ import android.os.Parcelable;
 
 public class Customer implements Parcelable {
     private int costumerNumber;
+    private String name;
     private String phoneNumber;
     private String address;
     private long created;
 
-    public Customer(int costumerNumber, String phoneNumber, String address) {
+    public Customer(int costumerNumber, String name, String phoneNumber, String address) {
         this.costumerNumber = costumerNumber;
+        this.name = name;
         this.phoneNumber = phoneNumber;
         this.address = address;
         this.created = System.currentTimeMillis();
@@ -40,6 +42,14 @@ public class Customer implements Parcelable {
         return address;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void setAddress(String address) {
         this.address = address;
     }
@@ -52,6 +62,7 @@ public class Customer implements Parcelable {
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeInt(costumerNumber);
+        dest.writeString(name);
         dest.writeString(phoneNumber);
         dest.writeString(address);
         dest.writeLong(created);
@@ -59,6 +70,7 @@ public class Customer implements Parcelable {
 
     private Customer(Parcel in) {
         costumerNumber = in.readInt();
+        name = in.readString();
         phoneNumber = in.readString();
         address = in.readString();
         created = in.readLong();
