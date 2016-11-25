04c5db0963ba679501a120fa318f30bd2ca95b6f
Erika Linde <erika.j.linde@gmail.com>
2016-11-25 19:28:12 +0100

    lagt till sökning av user i databasen

--- app/src/main/java/com/iths/grupp1/leveransapp/view/LoginActivity.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/view/LoginActivity.java
@@ -3,23 +3,26 @@ package com.iths.grupp1.leveransapp.view;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.iths.grupp1.leveransapp.R;
+import com.iths.grupp1.leveransapp.database.OrderSQLiteOpenHelper;
+import com.iths.grupp1.leveransapp.model.User;
 
 public class LoginActivity extends AppCompatActivity {
-    private static String correctUsername = "user";
-    private static String correctPassword = "pass";
     private TextView textViewUsernameMessage;
     private TextView textViewPasswordMessage;
-    private TextView textViewErrorMessage;
     private EditText username;
     private EditText password;
     private static String enteredPassword;
     private static String enteredUsername;
+    private OrderSQLiteOpenHelper users;
+    public static String retrievedUsername;
+    public static String retrievedPassword;
     //Testbutton for Orders.
     private Button loginBtn;
 
@@ -29,13 +32,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         textViewUsernameMessage = (TextView)findViewById(R.id.error_username);
         textViewPasswordMessage = (TextView)findViewById(R.id.error_password);
-        textViewErrorMessage = (TextView)findViewById(R.id.error);
         username = (EditText)findViewById(R.id.username);
         password = (EditText)findViewById(R.id.password);
+
         //Testbutton for Orders.
+        users = new OrderSQLiteOpenHelper(this);
         loginBtn = (Button) findViewById(R.id.login_btn);
+
     }
 
+
+    @Override
+    protected void onStart() {
+        deleteMessage();
+        super.onStart();
+    }
 
     public void goToOrders(View view) {
         enteredUsername = username.getText().toString();
@@ -45,7 +56,26 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void evaluateLogInData(){
-        if(correctUsername.equals(enteredUsername) && correctPassword.equals(enteredPassword)){
+
+        User user = users.getUser(enteredUsername);
+
+        if(user == null){
+            retrievedUsername = "not found";
+            retrievedPassword = "not found";
+        }else{
+            retrievedUsername = user.getUsername();
+            retrievedPassword = user.getPassword();
+        }
+
+        evaluateUsername();
+        evaluatePassword();
+
+        //Intent intent = new Intent(this, OrderListActivity.class);
+        //startActivity(intent);
+    }
+
+    public void evaluateUsername(){
+        if(retrievedUsername.equals(enteredUsername) && retrievedPassword.equals(enteredPassword)){
             Intent intent = new Intent(this, OrderListActivity.class);
             startActivity(intent);
         }
@@ -53,17 +83,27 @@ public class LoginActivity extends AppCompatActivity {
         if(enteredUsername.isEmpty()){
             textViewUsernameMessage.setText("Enter username");
         }
-        else if(!enteredUsername.equals(correctUsername)){
+        else if(!enteredUsername.equals(retrievedUsername)){
             textViewUsernameMessage.setText("Not valid username");
+            textViewPasswordMessage.setText(" ");
         }
+    }
 
+    public void evaluatePassword(){
         if(enteredPassword.isEmpty()){
             textViewPasswordMessage.setText("Enter password");
         }
-        else if(!enteredPassword.equals(correctPassword)){
+        else if(retrievedPassword.equals("not found")){
+            textViewPasswordMessage.setText(" ");
+        }
+        else if(!enteredPassword.equals(retrievedPassword)){
             textViewPasswordMessage.setText("Not valid password");
         }
-        Intent intent = new Intent(this, OrderListActivity.class);
-        startActivity(intent);
     }
+
+    public void deleteMessage(){
+        textViewUsernameMessage.setText(" ");
+        textViewPasswordMessage.setText(" ");
+    }
+
 }
