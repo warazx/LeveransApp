5391b01e69274454bdd94b19654fc7c060807367
fabianmikaelsson <fabian.mikaelsson@hotmail.com>
2016-11-28 09:20:22 +0100

    Added sharedpreferences to save orders per page and phonenr

--- app/src/main/java/com/iths/grupp1/leveransapp/view/SettingsActivity.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/view/SettingsActivity.java
@@ -1,8 +1,10 @@
 package com.iths.grupp1.leveransapp.view;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextSwitcher;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.iths.grupp1.leveransapp.R;
 
@@ -19,7 +22,7 @@ public class SettingsActivity extends AppCompatActivity {
     private TextView ordersPerPage;
     private EditText changePhonenumber;
     private Button changePhoneNumberBtn;
-    private String phoneNumber;
+    private String phoneNumber = "";
     //private Intent ordersPerPageIntent;
 
     @Override
@@ -34,8 +37,30 @@ public class SettingsActivity extends AppCompatActivity {
         changePhoneNumberBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                phoneNumber = changePhonenumber.getText().toString();
-                showAlert(this);
+                if(phoneNumber.equals(changePhonenumber.getText().toString())||changePhonenumber.getText().equals("")) {
+                    showAlert(this, "Skriv in ditt nya nummer");
+                }
+                else {
+                    phoneNumber = changePhonenumber.getText().toString();
+                    saveInfo(view);
+                    changePhoneNumberBtn.setEnabled(true);
+
+                    //changePhonenumber.clearFocus();
+                    changePhoneNumberBtn.requestFocus();
+                }
+
+
+                /*if(changePhonenumber.getText().length() != 10) {
+                    showAlert(this, "Numret måste innehålla 10 siffror");
+                }
+                else if (changePhonenumber.getText().charAt(0) == '+') {
+                    showAlert(this, "Plus");
+                }
+                else  {
+                    phoneNumber = changePhonenumber.getText().toString();
+                    saveInfo(view);
+                }*/
+
             }
         });
 
@@ -54,7 +79,7 @@ public class SettingsActivity extends AppCompatActivity {
                     @Override
                     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                         if (progress < 1) {
-                            seekBar.setProgress(1); // magic solution, ha
+                            seekBar.setProgress(1);
                         }
                         progressValue = progress+1;
                         ordersPerPage.setText(String.valueOf(progressValue));
@@ -68,15 +93,19 @@ public class SettingsActivity extends AppCompatActivity {
 
                     @Override
                     public void onStopTrackingTouch(SeekBar seekBar) {
-
+                        saveInfo(seekBar);
                     }
                 }
         );
     }
 
-    public void showAlert(View.OnClickListener v) {
+    public void validatePhoneNumber() {
+
+    }
+
+    public void showAlert(View.OnClickListener v, String errorMessage) {
         AlertDialog.Builder alertNr = new AlertDialog.Builder(this);
-        alertNr.setMessage("Skriv in det nya numret")
+        alertNr.setMessage(errorMessage)
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -86,4 +115,23 @@ public class SettingsActivity extends AppCompatActivity {
                 .create();
         alertNr.show();
     }
+
+    public void saveInfo(View v) {
+        SharedPreferences sharedPref = getSharedPreferences("userSettings", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("ordersPerPage", ordersPerPage.getText().toString());
+        editor.putString("phoneNumber", changePhonenumber.getText().toString());
+
+        editor.apply();
+        Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
+    }
+
+    public void displayData(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("userSettings", Context.MODE_PRIVATE);
+
+        String orders = sharedPref.getString("ordersPerPage", "");
+        String number = sharedPref.getString("phoneNumber", "");
+    }
+
 }
