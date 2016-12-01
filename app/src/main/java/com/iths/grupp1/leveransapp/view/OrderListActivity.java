9474f3420f484b4550dc5e667feb5368d6ffd9ed
Erika Linde <erika.j.linde@gmail.com>
2016-12-01 18:40:07 +0100

    Ful men funktionsduglig actionbar med switch och knapp för att lägga till ordrar

--- app/src/main/java/com/iths/grupp1/leveransapp/view/OrderListActivity.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/view/OrderListActivity.java
@@ -7,7 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.iths.grupp1.leveransapp.R;
 import com.iths.grupp1.leveransapp.adapter.OrderAdapter;
@@ -46,17 +51,54 @@ public class OrderListActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.activity_order_list_menu, menu);
+
+        MenuItem itemSwitch = menu.findItem(R.id.mySwitch);
+        itemSwitch.setActionView(R.layout.use_switch);
+        final Switch sw = (Switch) menu.findItem(R.id.mySwitch).getActionView().findViewById(R.id.switch1);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    historyOrders();
+                }else{
+                    updateOrders();
+                }
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.settings:
+                goToSettings();
+                //Intent intent = new Intent(this, SettingsActivity.class);
+                //startActivity(intent);
+                break;
+            case R.id.addOrders:
+                addOrders();
+                Log.d("TAG","Lägger till ordrar");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     //TODO: Move this to the toolbar.
     /**
      * Adds x orders to the database and updates the list. Number of orders is determined
      * by the the ordersPerPage setting in the SettingsActivity.
      * @param view
      */
-    public void addOrders(View view) {
+    public void addOrders() {
         String str = sharedPref.getString("ordersPerPage", "10");
         int amount = Integer.parseInt(str);
         GenerateDatabaseObject.addOrders(this, amount);
-        updateOrders(view);
+        updateOrders();
     }
 
     //  TODO: Move this to the toolbar.
@@ -64,7 +106,7 @@ public class OrderListActivity extends AppCompatActivity {
      * Gets all undelivered orders from the database and updates the list with them.
      * @param view
      */
-    public void updateOrders(View view) {
+    public void updateOrders() {
         OrderSQLiteOpenHelper db = new OrderSQLiteOpenHelper(this);
         orders = db.getUndeliveredOrders();
         swapOrders();
@@ -75,7 +117,7 @@ public class OrderListActivity extends AppCompatActivity {
      * Gets all delivered orders from the database and updates the list with them.
      * @param view
      */
-    public void historyOrders(View view) {
+    public void historyOrders() {
         OrderSQLiteOpenHelper db = new OrderSQLiteOpenHelper(this);
         orders = db.getDeliveredOrders();
         swapOrders();
@@ -86,7 +128,7 @@ public class OrderListActivity extends AppCompatActivity {
      * Sends the user to the SettingsActivity.
      * @param view
      */
-    public void goToSettings(View view) {
+    public void goToSettings() {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
