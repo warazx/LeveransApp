d9161c5f38db2939384f22891da0a04a79d921f6
Christian Karlsson <krille.karlsson@gmail.com>
2016-12-04 14:31:45 +0100

    Added permission handling for sending SMS.

--- app/src/main/java/com/iths/grupp1/leveransapp/view/OrderActivity.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/view/OrderActivity.java
@@ -1,10 +1,14 @@
 package com.iths.grupp1.leveransapp.view;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.util.Log;
@@ -21,7 +25,13 @@ import com.iths.grupp1.leveransapp.database.OrderSQLiteOpenHelper;
 import com.iths.grupp1.leveransapp.model.Customer;
 import com.iths.grupp1.leveransapp.model.Order;
 
+/**
+ * Show info about one specific order. Has different views depending on if the order
+ * has been delivered or not.
+ */
 public class OrderActivity extends AppCompatActivity {
+
+    private static final int SEND_SMS_PERMISSION = 1;
 
     private TextView orderIdText;
     private TextView placedDateText;
@@ -34,6 +44,7 @@ public class OrderActivity extends AppCompatActivity {
     private Order order;
     private Customer customer;
 
+    private SmsManager smsManager;
     private SharedPreferences sharedPref;
     private final String TAG = "TAG";
 
@@ -53,6 +64,7 @@ public class OrderActivity extends AppCompatActivity {
         phoneNumberText = (TextView) findViewById(R.id.order_activity_phone_value);
         deliveredDateText = (TextView) findViewById(R.id.activity_order_delivered_date_value);
         deliveryBtn = (Button) findViewById(R.id.order_activity_delivery_btn);
+        smsManager = SmsManager.getDefault();
 
         sharedPref = getSharedPreferences(SettingsActivity.STATUS_USER_SETTINGS, Context.MODE_PRIVATE);
 
@@ -96,6 +108,9 @@ public class OrderActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /*
+    Toggles between the views depending on if the order has been delivered or not.
+     */
     private void toggleLayout() {
         if(order.isDelivered()) {
             findViewById(R.id.ll_delivered).setVisibility(View.VISIBLE);
@@ -109,7 +124,6 @@ public class OrderActivity extends AppCompatActivity {
     /**
      * Changes the order to delivered. Updates the database with the location and changes
      * the display to match an delivered order.
-     * @param view Button.
      */
     public void deliverOrder(View view) {
         order.setDelivered(true);
@@ -125,34 +139,66 @@ public class OrderActivity extends AppCompatActivity {
         OrderSQLiteOpenHelper db = new OrderSQLiteOpenHelper(this);
         db.updateOrder(order);
 
-        sendSms();
+        sendSMS();
     }
 
-    /**
+    /*
      * Sends sms to user when an order is delivered.
      */
-    private void sendSms() {
-
-
-        String phoneNumber = sharedPref.getString(SettingsActivity.PHONE_NUMBER_TO_ADD, String.valueOf(R.string.empty_string));
-        String message = "Order " + orderIdText + " har levererats!";
-
-        SmsManager manager = SmsManager.getDefault();
-        try {
-            manager.sendTextMessage(phoneNumber, null, message, null, null);
-        } catch (Exception e) {
-            String errorMessage = "SMS not delivered. " + phoneNumber + " is not a valid phone number";
-            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+    private void sendSMS() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, SEND_SMS_PERMISSION);
+            }
+        } else {
+            sendSmsConfirmation();
         }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case SEND_SMS_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    sendSmsConfirmation();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    Toast.makeText(this, "Need permission to use service", Toast.LENGTH_SHORT).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void sendSmsConfirmation() {
+        String phoneNumber = sharedPref.getString(SettingsActivity.PHONE_NUMBER_TO_ADD, "0");
+        String message = String.format(getString(R.string.activity_order_sms_message), order.getOrderNumber());
+
+        try {
+            smsManager.sendTextMessage(phoneNumber, "", message, null, null);
+        } catch (IllegalArgumentException iae) {
+            iae.printStackTrace();
+            Toast.makeText(this, "Invalid phone number, please change in settings.", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "SMS could not be sent", Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
      * Starts Google Maps Navigation from the user's current location to the current order customer address."
-     * @param view Button.
      */
     public void startNavigation(View view) {
-
         String address = customer.getAddress();
         Uri uri = Uri.parse("google.navigation:q=" + address + "&mode=d");
         Intent intent = new Intent(Intent.ACTION_VIEW,uri);
