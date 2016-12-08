4addb5e3bc79ff728290273b059d817137c2dedc
Christian Karlsson <krille.karlsson@gmail.com>
2016-12-08 14:55:53 +0100

    The QR scanner no longer crashes upon invalid QR code.

--- app/src/main/java/com/iths/grupp1/leveransapp/view/QRScanActivity.java
+++ app/src/main/java/com/iths/grupp1/leveransapp/view/QRScanActivity.java
@@ -7,8 +7,15 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.google.zxing.Result;
+import com.iths.grupp1.leveransapp.R;
+import com.iths.grupp1.leveransapp.database.OrderSQLiteOpenHelper;
+import com.iths.grupp1.leveransapp.model.Customer;
+import com.iths.grupp1.leveransapp.model.Order;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import static com.iths.grupp1.leveransapp.adapter.OrderAdapter.EXTRA_CUSTOMER;
+import static com.iths.grupp1.leveransapp.adapter.OrderAdapter.EXTRA_ORDER;
 
 public class QRScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
@@ -42,18 +49,29 @@ public class QRScanActivity extends AppCompatActivity implements ZXingScannerVie
     public void handleResult(Result result) {
         // Do something with the result here
         String value = result.getText();
-        Log.v(TAG, value); // Prints scan results
-        String typeOfBarcode = result.getBarcodeFormat().toString();
-        Log.v(TAG, typeOfBarcode); // Prints the scan format (qrcode, pdf417 etc.)
+        int intValue;
+        try {
+            intValue = Integer.parseInt(value);
+            Log.v(TAG, value); // Prints scan results
+            String typeOfBarcode = result.getBarcodeFormat().toString();
+            Log.v(TAG, typeOfBarcode); // Prints the scan format (qrcode, pdf417 etc.)
 
-        if(!result.getText().isEmpty()) {
-            String message = String.format("Order %s found!", result.getText());
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            OrderSQLiteOpenHelper db = new OrderSQLiteOpenHelper(this);
+            Order order = db.getOrder(intValue);
 
-            Intent intent = new Intent(this, OrderActivity.class);
-            intent.putExtra(ORDER_ID_KEY, Integer.parseInt(result.getText()));
-            startActivity(intent);
-        }
+            if (order != null) {
+                String message = String.format(getString(R.string.activity_qr_scan_order_found_toast), result.getText());
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+                Customer customer = db.getCustomer(order.getCustomer());
+
+                Intent intent = new Intent(this, OrderActivity.class);
+                intent.putExtra(EXTRA_ORDER, order);
+                intent.putExtra(EXTRA_CUSTOMER, customer);
+                startActivity(intent);
+            }
+        } catch (Exception e) {
+        Toast.makeText(this, R.string.activity_qr_scan_invalid_order_toast, Toast.LENGTH_SHORT).show();
+    }
 
         // If you would like to resume scanning, call this method below:
         scannerView.resumeCameraPreview(this);
